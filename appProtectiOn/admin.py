fa3e1a1de71184606fe5fb2e12d9d8585bb28279
ignaciogonlez <ignacio.gonlez@opendeusto.es>
2025-05-07 10:50:25 +0200

    alertas en admin

--- appProtectiOn/admin.py
+++ appProtectiOn/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
+# appProtectiOn/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Alerta
+
+@admin.register(Alerta)
+class AlertaAdmin(admin.ModelAdmin):
+    list_display    = ('id', 'usuario', 'lat', 'lng', 'audio', 'timestamp')
+    readonly_fields = ('timestamp',)
